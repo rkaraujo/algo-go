3bad874e6c90bff7ba0b7d8b10f339804e7b3d84
rkaraujo <rkaraujo@gmail.com>
2021-10-08 07:03:41 -0300

    binary tree search

---
+++ 0700_binary_search_tree/go.mod
@@ -0,0 +1,3 @@
+module algo/0700_binary_search_tree
+
+go 1.17
---
+++ 0700_binary_search_tree/solution.go
@@ -0,0 +1,31 @@
+package solution0700
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func searchBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+
+	if root.Val == val {
+		return root
+	}
+
+	if val < root.Val {
+		return searchBST(root.Left, val)
+	} else {
+		return searchBST(root.Right, val)
+	}
+}

cc11b0c6d35955f7b1475d738aa8e8e10dcb7a15
rkaraujo <rkaraujo@gmail.com>
2021-10-08 07:17:02 -0300

    insert binary search tree

---
+++ 0701_insert_binary_search_tree/go.mod
@@ -0,0 +1,3 @@
+module algo/0701_insert_binary_search_tree
+
+go 1.17
---
+++ 0701_insert_binary_search_tree/solution.go
@@ -0,0 +1,29 @@
+package solution0701
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{val, nil, nil}
+	}
+
+	if val < root.Val {
+		root.Left = insertIntoBST(root.Left, val)
+	} else {
+		root.Right = insertIntoBST(root.Right, val)
+	}
+
+	return root
+}

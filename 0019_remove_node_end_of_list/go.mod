295765deb4319c1d645c0c40d2c0c55109eaf8dc
rkaraujo <rkaraujo@gmail.com>
2021-10-13 07:59:28 -0300

    remove node end of list

---
+++ 0019_remove_node_end_of_list/go.mod
@@ -0,0 +1,3 @@
+module algo/0019_remove_node_end_of_list
+
+go 1.17
---
+++ 0019_remove_node_end_of_list/solution.go
@@ -0,0 +1,36 @@
+package solution0019
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	cur := head
+	end := head
+
+	// create a n distance between cur and end
+	for i := 0; i < n; i++ {
+		end = end.Next
+	}
+
+	// go to end of list
+	for end.Next != nil {
+		cur = cur.Next
+		end = end.Next
+	}
+
+	// remove next
+	if cur.Next != nil {
+		cur.Next = cur.Next.Next
+	}
+
+	return head
+}

7d73efe6859e36ffb9b93ef1b6fbed1e61ed3947
rkaraujo <rkaraujo@gmail.com>
2021-10-14 07:49:15 -0300

    permutation in string

--- 0567_permutation_in_string/solution.go
+++ 0567_permutation_in_string/solution.go
@@ -1,28 +1,42 @@
 package solution0567
 
 func checkInclusion(s1 string, s2 string) bool {
-	lenS1 := len(s1)
+	if len(s2) < len(s1) {
+		return false
+	}
 
-	for i := 0; i+lenS1 <= len(s2); i++ {
-		if checkPermutation(s1, s2[i:i+lenS1]) {
+	// store s1 count
+	var s1count [26]int
+	for i := 0; i < len(s1); i++ {
+		s1count[s1[i]-'a']++
+	}
+
+	// store s2 window count
+	var s2windowcount [26]int
+	for i := 0; i < len(s1); i++ {
+		s2windowcount[s2[i]-'a']++
+	}
+
+	// check every window
+	for i := 0; i < len(s2)-len(s1); i++ {
+		if isSameCount(s1count, s2windowcount) {
 			return true
 		}
+
+		// reduce last char count
+		s2windowcount[s2[i]-'a']--
+		// add next char count
+		s2windowcount[s2[i+len(s1)]-'a']++
 	}
 
-	return false
+	return isSameCount(s1count, s2windowcount)
 }
 
-func checkPermutation(s1 string, subStringS2 string) bool {
-	mapS1 := make(map[byte]int)
-	for i := 0; i < len(s1); i++ {
-		mapS1[s1[i]]++
-	}
-
-	for i := 0; i < len(subStringS2); i++ {
-		if mapS1[subStringS2[i]] == 0 {
+func isSameCount(s1count, s2count [26]int) bool {
+	for i := 0; i < 26; i++ {
+		if s1count[i] != s2count[i] {
 			return false
 		}
-		mapS1[subStringS2[i]]--
 	}
 	return true
 }
